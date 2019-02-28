256aeb937e89aa2adf71d00f717d6eed3d542473
NataliaMP <48107615+NataliaMP@users.noreply.github.com>
2019-02-28 18:00:16 -0500

    Update excel.py

--- excel.py
+++ excel.py
@@ -3,7 +3,7 @@ import xlrd
 workbook = xlrd.open_workbook("190219_serviciosWRAmanualV5.xls")
 worksheet = workbook.sheet_by_name("ServiciosDWDM")
 
-workbook2 = xlrd.open_workbook("Potencia2.xlsx")
+workbook2 = xlrd.open_workbook("Transponders.xlsx")
 worksheet2 = workbook2.sheet_by_name("Hoja1")
 
 filas = worksheet.nrows
@@ -13,11 +13,14 @@ my_dict = {}
 
 for i in range(2, filas):
     Equipo_A = worksheet.cell(i, 12).value
-    Cliente_Shelf_Slot_A = worksheet.cell(i, 13).value
-    Linea_Shelf_Slot_A = worksheet.cell(i, 14).value
+    Client = worksheet.cell(i, 13).value
+    Line = worksheet.cell(i, 14).value
 
+    for i in range(0, filas2):
+        NEname = worksheet2.cell(x, 1).value
+        Transponder = worksheet.cell(x,2).value
 
  if Equipo_A == NEname:
- if Cliente_Shelf_Slot_A == Transponder:
+ if Client == Transponder:
  worksheet.cell(i,19).value = worksheet2.cell(x,8).value)
 
